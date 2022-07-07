4fe1ef7770b3baeea6e7dd124dd2b080b470eac3
Stef Castillo <estefan.castillo@yahoo.com>
2022-07-06 22:49:23 -0500

    first commit

---
+++ maxOfTwo.js
@@ -0,0 +1,11 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+if ( y > x ) {
+    console.log(`Y is`, (y))
+} else if (y === x) {
+    console.log("The values are identical")
+} else {
+    console.log(`X is`, (x))
+}
